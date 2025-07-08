c8c842078b66c6f340d2454d89ef4b23850e45e2
Sandro <alexanderkapanadze52@gmail.com>
2025-07-08 17:21:43 +0200

    more CORS tweaks

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,8 @@ async function bootstrap() {
     origin: [
       'https://mittweida-front.vercel.app',
       'http://localhost:5173',
-      '192.168.0.101:5173',
-      '80.241.200.66:5173',
+      'http://192.168.0.101:5173',
+      'http://80.241.200.66:5173',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
