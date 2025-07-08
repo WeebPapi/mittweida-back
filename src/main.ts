b515d325327694c965175540f34720f3a303a91d
Sandro <alexanderkapanadze52@gmail.com>
2025-07-08 14:49:28 +0200

    formatting fix for cors

--- src/main.ts
+++ src/main.ts
@@ -7,33 +7,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'https://mittweida-front-d7cl.vercel.app/',
+    origin: [
+      'https://mittweida-front-d7cl.vercel.app/',
+      'http://localhost:5173',
+      '192.168.0.101:5173',
+      '80.241.200.66:5173',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
-  //FOR DEV
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
-  // FOR DEV
-  app.enableCors({
-    origin: '192.168.0.101:5173',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
-  //FOR DEV
-  app.enableCors({
-    origin: '80.241.200.66:5173',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
