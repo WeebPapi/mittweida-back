69e7e3d3d6278e65c33e602be2118d4d53541357
Sandro <alexanderkapanadze52@gmail.com>
2025-07-16 21:41:12 +0200

    more cors debugging

--- src/main.ts
+++ src/main.ts
@@ -9,18 +9,19 @@ async function bootstrap() {
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: [
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  /*[
       'https://weebpapi.github.io/',
       'https://localhost:5173',
       'https://172.17.212.53:5173',
       'https://192.168.0.101:5173',
       'https://80.241.200.66:5173',
       'http://localhost:5173',
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+    ]*/
 
   const config = new DocumentBuilder()
     .setTitle('Mittweida Api')
